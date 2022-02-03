dbb4f0a90e3fde70880dd8e92cf71d4dbdc44824
Michele Memoli <michele@100shapes.com>
2022-02-03 18:34:49 +0000

    renamed some files

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,1 +1,1 @@
-module.exports = require("config/eslint-preset");
+module.exports = require("@myturborepo/config/eslint-preset");
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "ui";
+import { Button } from "@myturborepo/ui";
 
 export default function Web() {
   return (
